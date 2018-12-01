872848c84a6be38a71638865bb703a96faa6ebe8
Hugo Mansilla <hugomansilla@gmail.com>
2018-11-30 21:17:16 -0300

    casi listo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,5 +8,24 @@ $(document).ready(function(){
 			$('.navbar').removeClass('menu-fixed');
 		}
 	});
+
+	$('.grid').masonry({
+		  // options
+		  itemSelector: '.grid-item',
+		  columnWidth: 220
+		});
  
+});
+
+$(document).ready(function(){
+	$.smallBox({
+	  position:3,
+	  title:"Despacho gratuito",
+	  content:"Por compras superiores a $20.000.",
+	  width:280,
+	  img:"assets/img/despacho.jpg",
+	  sound:false,
+	  color:"#63B999",
+	});
+
 });
